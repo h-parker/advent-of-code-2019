b13e1ef823955ddf64787073606733a0dbaf1ab9
Hannah Parker <hparker5@u.rochester.edu>
2020-01-03 14:10:54 -0500

    add opcodes 3 and 4 to module

--- Notebooks/opcodes.py
+++ Notebooks/opcodes.py
@@ -1,16 +1,51 @@
-def one(program, idx, param_modes):
-	a, b = get_first_two_params(program, param_modes, idx)
-	program[program[idx+3]] = a + b
-	idx += 4
-	return program, idx
+"""
+This module contains opcode functions and helper functions needed
+"""
 
-def two(program, idx, param_modes):
-	a, b = get_first_two_params(program, param_modes, idx)
-	program[program[idx+3]] = a * b
-	idx += 4
-	return program, idx
+def one(memory, pointer, param_modes):
+	"""
+	Addition
+	"""
+	a, b = get_first_two_params(memory, param_modes, pointer)
+	memory[memory[pointer+3]] = a + b
+	address += 4
+	return memory, pointer
 
-def get_first_two_params(program, param_modes, idx):
-	a = program[program[idx + 1]] if param_modes[0] == 0 else program[idx + 1]
-	b = program[program[idx + 2]] if param_modes[1] == 0 else program[idx + 2]
+def two(memory, pointer, param_modes):
+	"""
+	Multiplication
+	"""
+	a, b = get_first_two_params(memory, param_modes, pointer)
+	memory[memory[pointer+3]] = a * b
+	pointer += 4
+	return memory, pointer
+
+def three(memory, pointer, user_input):
+	"""
+	Saves user input to a given address. Does not use parameter modes
+	because the only parameter is the address the user input is saved to
+	"""
+	if not user_input:
+		user_input = input('Please input a value. ')
+
+	memory[pointer + 1] = user_input
+	pointer += 2
+	return memory, pointer
+
+def four(memory, pointer, param_modes):
+	"""
+	Outputing value in memory
+	"""
+	output = memory[memory[pointer + 1]] if param_modes[0] == 0 else memory[pointer + 1]
+	print(output)
+	pointer += 2
+	return output
+
+def get_first_two_params(memory, param_modes, pointer):
+	"""
+	Returns the values to be used according to the parameter modes provided
+	(helper function)
+	"""
+	a = memory[memory[pointer + 1]] if param_modes[0] == 0 else memory[pointer + 1]
+	b = memory[memory[pointer + 2]] if param_modes[1] == 0 else memory[pointer + 2]
 	return a, b
