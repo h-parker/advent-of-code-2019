cae3dad802b5716d46e63e8c0ea62171f0dba291
Hannah Parker <hparker5@u.rochester.edu>
2020-01-06 17:52:19 -0500

    made opcodes get_two_params more general - get any number of params

--- Notebooks/opcodes.py
+++ Notebooks/opcodes.py
@@ -6,8 +6,8 @@ def one(memory, pointer, param_modes):
 	"""
 	Addition
 	"""
-	a, b = get_first_two_params(memory, pointer, param_modes)
-	memory[memory[pointer+3]] = a + b
+	param = get_params(memory, pointer, param_modes)
+	memory[memory[pointer + 3]] = param[0] + param[1]
 	pointer += 4
 	return memory, pointer
 
@@ -15,8 +15,8 @@ def two(memory, pointer, param_modes):
 	"""
 	Multiplication
 	"""
-	a, b = get_first_two_params(memory, pointer, param_modes)
-	memory[memory[pointer+3]] = a * b
+	param = get_params(memory, pointer, param_modes)
+	memory[memory[pointer + 3]] = param[1] * param[2]
 	pointer += 4
 	return memory, pointer
 
@@ -36,16 +36,54 @@ def four(memory, pointer, param_modes):
 	"""
 	Outputing value in memory
 	"""
-	output = memory[memory[pointer + 1]] if param_modes[0] == 0 else memory[pointer + 1]
+	param = get_params(memory, pointer, param_modes)
+	output = param[0]
 	print(output)
 	pointer += 2
 	return memory, pointer, output
 
-def get_first_two_params(memory, pointer, param_modes):
+def five(memory, pointer, param_modes):
+	param = get_params(memory, pointer, param_modes)
+	if param[0] != 0:
+		pointer = param[1]
+	else:
+		pointer += 3
+	return memory, pointer
+
+def six(memory, pointer, param_modes):
+	param = get_params(memory, pointer, param_modes)
+	if param[0] == 0:
+		pointer = param[1]
+	else:
+		pointer += 3
+	return memory, pointer
+
+def seven(memory, pointer, param_modes):
+	param = get_params(memory, pointer, param_modes)
+	if param[0] < param[1]:
+		memory[memory[pointer + 3]] = 1
+	else:
+		memory[memory[pointer + 3]] = 0
+	pointer += 4
+	return memory, pointer
+
+def eight(memory, pointer, param_modes):
+	param = get_params(memory, pointer, param_modes)
+	if param[0] == param[1]:
+		memory[memory[pointer + 3]] = 1
+	else:
+		memory[memory[pointer + 3]] = 0
+	pointer += 4
+	return memory, pointer
+
+def get_params(memory, pointer, param_modes):
 	"""
 	Returns the values to be used according to the parameter modes provided
 	(helper function)
 	"""
-	a = memory[memory[pointer + 1]] if param_modes[0] == 0 else memory[pointer + 1]
-	b = memory[memory[pointer + 2]] if param_modes[1] == 0 else memory[pointer + 2]
-	return a, b
+	parameters = []
+	i = 1
+	for mode in param_modes:
+		parameters.append(memory[memory[pointer + i]] if param_modes[0] == 0 else memory[pointer + i])
+		i += 1
+	return parameters
