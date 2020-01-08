2451e8088ab22bdd2a7898297422c7bf7d55c5d0
Hannah Parker <hparker5@u.rochester.edu>
2020-01-08 14:02:33 -0500

    update intcode class to add paused and automate variables to automate chained computers

--- Notebooks/Intcode.py
+++ Notebooks/Intcode.py
@@ -5,8 +5,9 @@ class Intcode:
 	Creating an instance of the Intcode class will create a computer that can be used to read
 	and execute Intcode programs. 
 	"""
-	def __init__(self, program, pointer = 0, user_input = False, replacements = False):
+	def __init__(self, name, program, pointer = 0, user_input = False, replacements = False):
 		"""
+		- name is mostly here for debugging purposes
 		- program is the sequence of integers containing opcodes the integers they operate on.
 		- pointer is the address in memory the Intcode computer is currently operating on; 
 		defaults to 0, since a new instance of an intcode computer will start at the beginning
@@ -17,12 +18,20 @@ class Intcode:
 		in the value in address 0 being replaced with 3 and the value in address 10 being
 		replaced with 33 in whatever the program is. 
 		"""
+		self.name = name
 		self.memory = program.copy() # set initial memory
 		self.pointer = pointer
 		self.user_input = user_input
-		self.replacements = replacements
+		self.replacements = replacements 
 		self.halted = False # True once opcode 99 is reached
-		self.curr_output = None # store last output from opcode 4
+		self.curr_output = False # store last output from opcode 4
+		self.paused = False # pause to wait to get input
+
+		# False if you want to have the computer wait to recieve human input and True
+		# if you are giving the computer input and it's operating on it's own, going
+		# through the input supplied (or waiting for another computer to supply input)
+		self.automate= bool(user_input)
+		
 
 	def intcode(self):
 		"""
@@ -35,18 +44,20 @@ class Intcode:
 
 		# get initial program 
 		opcode, param_modes = self.__interpret_instructions__(self.memory[self.pointer])
-		while not self.halted and self.pointer < len(self.memory):
+		while not self.halted and not self.paused and self.pointer < len(self.memory):
 			if opcode == 99:
 				self.halted = True
 			else:
-				if opcode != 4:
+				if opcode == 3:
+					self.memory, self.pointer, self.paused = self.__execute_opcode__(opcode, param_modes)
+					# get rid of last used input if there was input provided
+					if not self.paused:
+						self.user_input.pop(0)
+
+				elif opcode != 4:
 					self.memory, self.pointer = self.__execute_opcode__(opcode, param_modes)
 				else:
 					self.memory, self.pointer, self.curr_output = self.__execute_opcode__(opcode, param_modes)
-
-				#get rid of last used input, if the opcode is 3
-				if opcode == 3:
-					self.user_input.pop(0)
 
 				opcode, param_modes = self.__interpret_instructions__(self.memory[self.pointer])
 
@@ -76,7 +87,6 @@ class Intcode:
 		else:
 			param_modes = []
 
-
 		return opcode, param_modes
 
 
@@ -88,13 +98,15 @@ class Intcode:
 		"""
 		# we use lambdas so that python lazily evaluates these functions -- a function will 
 		# only be evaluated when called in the return statement
-		# print(opcode)
-		# print(self.memory)
-		# print(param_modes)
+		# print('computer', self.name, 'opcode:', opcode)
+		# print('computer', self.name, 'pointer', self.pointer)
+		# print('computer', self.name, 'memory:', self.memory)
+		# print('computer', self.name, 'parameter modes:', param_modes)
 		opcode_dict = {
 			1: (lambda: opcodes.one(self.memory, self.pointer, param_modes)),
 			2: (lambda: opcodes.two(self.memory, self.pointer, param_modes)),
-			3: (lambda: opcodes.three(self.memory, self.pointer, self.user_input)),
+			3: (lambda: opcodes.three(self.memory, self.pointer, self.user_input, 
+				self.automate, self.paused)),
 			4: (lambda: opcodes.four(self.memory, self.pointer, param_modes)),
 			5: (lambda: opcodes.five(self.memory, self.pointer, param_modes)),
 			6: (lambda: opcodes.six(self.memory, self.pointer, param_modes)),
